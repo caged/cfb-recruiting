eaceeed0f1582a8a4c5a73b25750fa8b00b72802
Justin Palmer <justin@github.com>
2013-11-06 18:55:40 -0800

    update counties topojson with total recruits

--- script/count-queries.sql
+++ script/count-queries.sql
@@ -15,10 +15,12 @@ COPY (SELECT cfb_counties.geoid, cfb_counties.gisjoin, cfb_counties.name,
   SUM(CAST((stars = 3) AS INT)) as three_star,
   SUM(CAST((stars = 2) AS INT)) as two_star,
   SUM(CAST((stars = 1) AS INT)) as one_star,
+  COUNT(1) as total,
   (max(h76007) + max(h76008) + max(h76009) + max(h76010)) as male_18_24
 FROM cfb_counties
 INNER JOIN recruits ON st_contains(cfb_counties.geom, recruits.geom)
 LEFT JOIN cfb_counties_data on cfb_counties.gisjoin = cfb_counties_data.gisjoin
+WHERE stars > 0
 GROUP BY cfb_counties.name, cfb_counties.gisjoin, cfb_counties.geoid)
 TO '/tmp/cfb-counties.csv' WITH CSV HEADER;
 
